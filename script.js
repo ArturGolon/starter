d188a8044fd9c5fe39043fc425b7db4ba7ceb73b
ArturGolon <golonarthur@gmail.com>
2019-12-14 18:38:24 +0100

    coding challenge 1 add weight mark and john

--- script.js
+++ script.js
@@ -128,6 +128,6 @@ var heightJohn = 1.95;
 
 var BMIMark = massMark / (heightMark * heightMark);
 var BMIJohn = massJohn / (heightJohn * massJohn);
-
+console.log("Mark " + BMIMark, " John " + BMIJohn);
 var markHigherBMI = BMIMark > BMIJohn;
 console.log("Is Mark's BMI higher than John's " + markHigherBMI);
