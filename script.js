2d7b686fb554e2ddcda3072162b785e0a7fb45a7
ArturGolon <golonarthur@gmail.com>
2019-12-14 20:23:23 +0100

    ternery operator and switch statment

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ if (BMIMark > BMIJohn) {
 */
 
 //BOOLEAN LOGIC
-
+/*
 var firstName = "Artur";
 var age = 21;
 
@@ -179,4 +179,55 @@ if (age < 13) {
   console.log(firstName + " is a young man");
 } else {
   console.log(firstName + " is a man");
+}
+*/
+
+//THE TERNERY OPERATOR AND SWITCH STATEMENTS
+
+var firstName = "Artur";
+var age = 14;
+//ternery operator
+age >= 18
+  ? console.log(firstName + " drinks beer.")
+  : console.log(firstName + " drinks juice.");
+
+var drink = age >= 18 ? "beer" : "juice";
+console.log(drink);
+/*
+if (age >= 18) {
+  var drink = "beer";
+} else {
+  var drink = "juice";
+}*/
+
+//switch statement
+
+var job = "teacher";
+switch (job) {
+  case "teacher":
+  case "instructor":
+    console.log(firstName + " teaches kids how to code.");
+    break;
+  case "driver":
+    console.log(firstName + "driver an uber in Poland");
+    break;
+  case "designer":
+    console.log(firstName + "designe cool website's");
+    break;
+  default:
+    console.log(firstName + " dows something else");
+}
+
+switch (true) {
+  case age < 13:
+    console.log(firstName + " is a boy");
+    break;
+  case age >= 13 && age < 20:
+    console.log(firstName + " is a teenager");
+    break;
+  case age >= 20 && age < 30:
+    console.log(firstName + " is a young man");
+    break;
+  default:
+    console.log(firstName + " is a man");
 }
